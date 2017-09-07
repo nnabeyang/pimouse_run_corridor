0047003dfb068f240f6e8390ecc5c6043837e166
Noriaki Watanabe <nabeyang@gmail.com>
2017-09-07 19:06:53 +0900

    rosrunを呼び出すオブジェクトを間違っていたので修正

--- test/travis_test_wall_stop.py
+++ test/travis_test_wall_stop.py
@@ -22,4 +22,4 @@ class WallStopTest(unittest.TestCase):
 if __name__ == '__main__':
   time.sleep(3)
   rospy.init_node('travis_test_wall_stop')
-  rospy.rosrun('pimouse_run_corridor', 'travis_test_wall_stop', WallStopTest)
+  rostest.rosrun('pimouse_run_corridor', 'travis_test_wall_stop', WallStopTest)
